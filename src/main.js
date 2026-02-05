92da087f47c4926a4953b278393031be2aa0c2c4
Ankitha Pai <ankithapai@ymail.com>
2026-02-04 21:16:30 -0800

    passage can be changed based on the options and css fixed around it

--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
-const passageElement = document.querySelector('.passageContent');
+
+const passagesJSON = await fetch('./data.json')
+  .then(response => response.json())
+
+
+let diifficultyOption = 'easy';
+
+const passageElement = document.querySelector('.passageContainer');
+let passageContent = passagesJSON[diifficultyOption][Math.floor(Math.random() * passagesJSON[diifficultyOption].length)];
+passageElement.innerHTML = passageContent.text;
+
+
+const diifficultyOptions = document.querySelector('.difficulty .options');
+diifficultyOptions.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('option')) {
+        e.target.classList.add('selected');
+
+        diifficultyOption = e.target.textContent.toLowerCase();
+        passageContent = passagesJSON[diifficultyOption][Math.floor(Math.random() * passagesJSON[diifficultyOption].length)];
+        passageElement.innerHTML = passageContent.text;
+
+        const allBtns = diifficultyOptions.querySelectorAll('.option');
+        allBtns.forEach(btn => {
+            if (btn !== e.target) {
+                btn.classList.remove('selected');
+            }   
+        });
+    }
+
+    console.log(diifficultyOption);
+})
+
 
